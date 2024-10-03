67477d52cc195f251b0272f53265dc7428644f4d
rahultrivedi14 <rahul.trivedi@gmail.com>
2024-10-02 19:28:50 -0700

    client stability against server shutdown#

--- CMakeProjectSLMclient.cpp
+++ CMakeProjectSLMclient.cpp
@@ -4,27 +4,60 @@
 
 #include "CMakeProjectSLMClient.h"
 using namespace std;
+#define RECV_TIMEOUT 1000
+#define SEND_TIMEOUT 1000
+
+static zmq::socket_t s_client_socket(zmq::context_t& context, std::string& reconnect_msg) {
+    zmq::socket_t client(context, zmq::socket_type::req);
+    zmq::message_t reply;
+    try {
+        client.connect("tcp://192.168.122.1:5555");
+        client.setsockopt(ZMQ_RCVTIMEO, RECV_TIMEOUT);
+        client.setsockopt(ZMQ_SNDTIMEO, SEND_TIMEOUT);
+        // send INIT to server - client has been initialized.
+        client.send(zmq::buffer(std::string("INIT")), zmq::send_flags::none);
+        client.recv(reply, zmq::recv_flags::none);
+        if (reply.to_string() == "ackINIT") {
+            std::cout << "Received: " << reply.to_string() << std::endl;
+            reconnect_msg = "ackINIT";
+        }
+        else {
+            std::cout << "Failed to receive ackINIT from server... " << std::endl;
+        }
+    }
+    catch (zmq::error_t &e) {
+        std::cout << "Failed to receive ackINIT from server... " << std::endl;
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    return client;
+}
 
 int main() {
     //  Prepare zmq context and socket
     zmq::context_t context;
-    zmq::socket_t socket(context, zmq::socket_type::req);
-    std::cout << "Connecting to hello world server..." << std::endl;
-    zmq::message_t reply;
     bool zmq_okay = false;
-    // send INIT to server - client has been initialized.
-    socket.connect("tcp://192.168.122.1:5555");
-    socket.send(zmq::buffer(std::string("INIT")), zmq::send_flags::none);
-    socket.recv(reply, zmq::recv_flags::none);
-    if (reply.to_string() == "ackINIT") {
-        std::cout << "Received ackINIT from server" << std::endl;
-        zmq_okay = true;
+    zmq::message_t reply;
+    std::cout << "Connecting to server..." << std::endl;
+    zmq::socket_t client(context, zmq::socket_type::req);
+    try {
+        client.connect("tcp://192.168.122.1:5555");
+        client.setsockopt(ZMQ_RCVTIMEO, RECV_TIMEOUT);
+        client.setsockopt(ZMQ_SNDTIMEO, SEND_TIMEOUT);
+        // send INIT to server - client has been initialized.
+        client.send(zmq::buffer(std::string("INIT")), zmq::send_flags::none);
+        client.recv(reply, zmq::recv_flags::none);
+        if (reply.to_string() == "ackINIT") {
+            std::cout << "Received ackINIT" << std::endl;
+            zmq_okay = true;
+        }
+        else {
+            std::cout << "Failed to receive ackINIT from server... " << std::endl;
+        }
     }
-    else {
-        std::cout << "Did not receive ackINIT from server" << std::endl;
-        return 2;
+    catch (zmq::error_t e) {
+        std::cout << "Error: " << e.what() << std::endl;
     }
-
+   
     // Construct a Blink_SDK instance with Overdrive capability.
     int board_number;
     unsigned int bits_per_pixel = 12U;
@@ -59,33 +92,82 @@ int main() {
         int width = sdk.Get_image_width(board_number);
         std::cout << "SLM Width = " << width << std::endl;
         std::cout << "SLM Height = " << height << std::endl;
+        int num_retries = 5;
 
         while (true) {
-            socket.send(zmq::buffer(std::string("xfer")), zmq::send_flags::none);
-            socket.recv(reply, zmq::recv_flags::none);
+            // zmq::pollitem_t items[] = { { client, 0, ZMQ_POLLIN, 0} };
+            // zmq::poll(&items[0], 1, REQUEST_TIMEOUT);
+
+            try {
+                client.send(zmq::buffer(std::string("ping")), zmq::send_flags::none);
+            }
+            catch (zmq::error_t &e) {
+                std::cout << "zmq send error: " << e.what() << std::endl;
+                std::string reconnect_msg = "";
+                    for (int i = 0; i <= num_retries; i++) {
+                        std::cout << "Retrying Connection..." << std::endl;
+                        client.close();
+                        client = s_client_socket(context, reconnect_msg);
+                        if (reconnect_msg == "ackINIT") {
+                            break;
+                        }
+                        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+                        if (i == num_retries) {
+                            std::cout << "Abondoning Connection" << std::endl;
+                            return -1;
+                        }
+                    }
+            }
+            try {
+                client.recv(reply, zmq::recv_flags::none);
+            }
+            catch (zmq::error_t &e) {
+                std::cout << "zmq recv error: " << e.what() << std::endl;
+                /* for (int i = 0; i <= 3; i++) {
+                    std::cout << "Retrying Connection..." << std::endl;
+                    // client.close();
+                    // client = s_client_socket(context);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                    if (i == 3) {
+                        std::cout << "Abondoning Connection" << std::endl;
+                        return -1;
+                    }
+                }*/
+            }
+ 
             auto reply_data = reply.data();
             std::size_t image_len = reply.size();
             // int element =  static_cast<int>(static_cast<uint8_t*>(reply_data)[0]);
-            std::vector<uint8_t> image(image_len);
-            std::memcpy(image.data(), reply_data, image_len);
-            // std::vector<uint8_t> image = static_cast<uint8_t*>(reply_data);
-
-            if (image_len > 0) {
+            // unsigned char* image = new unsigned char[width * height];
+            // auto image = std::make_unique<unsigned char[]>(image_len);
+ 
+            if (image_len == width * height) {
+                std::vector<uint8_t> image(image_len);
+                std::memcpy(image.data(), reply_data, image_len);
                 // std::cout << "Received " << image_len << " bytes: " << element << std::endl;
                 std::cout << "Received " << image_len << " bytes: " << static_cast<int>(image[0]) << " end " << static_cast<int>(image[9999]) << std::endl;
+                // Generate test mask
+                // unsigned char* imageOne = new unsigned char[width * height];
+                // int VortexCharge = 5;
+                // Generate_LG(imageOne, width, height, VortexCharge, width / 2.0, height / 2.0, false);
+                // Write test mask
+                bool ExternalTrigger = false;
+                bool OutputPulse = false;
+                bool slm_write_success = sdk.Write_image(board_number, image.data(), width * height, ExternalTrigger, OutputPulse, 5000);
+                if (slm_write_success) {
+                    std::cout << "SLM write success" << std::endl;
+                }
+                else {
+                    std::cout << "SLM write failed" << std::endl;
+                }
+                image.clear();
+            }
+            else if (reply.to_string() == "pong") {
+                std::cout << "pong..." << std::endl;
             }
             else {
-                std::cout << "Received empty mask" << std::endl;
+                std::cout << "Waiting to receive mask..." << std::endl;
             }
-            // Generate test mask
-            // unsigned char* imageOne = new unsigned char[width * height];
-            // int VortexCharge = 5;
-            // Generate_LG(imageOne, width, height, VortexCharge, width / 2.0, height / 2.0, false);
-            // Write test mask
-            bool ExternalTrigger = false;
-            bool OutputPulse = false;
-            // sdk.Write_image(board_number, image, width * height, ExternalTrigger, OutputPulse, 5000);
-            delete[]image;
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
         sdk.SLM_power(false);
